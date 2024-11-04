328eb5b70fda283241b61dbf18ca010ff6e2985a
kshetritej <tejghartikshetri@gmail.com>
2024-11-04 20:13:40 +0545

    update: packages && add: tanstack router

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPath from "vite-tsconfig-paths";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPath from 'vite-tsconfig-paths'
+import viteReact from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [
-          ["@babel/plugin-proposal-decorators", { legacy: true }],
-          ["@babel/plugin-transform-class-properties"],
-        ],
-      },
-    }),
-    tsconfigPath(),
-  ],
-});
+  plugins: [viteReact(), TanStackRouterVite(), tsconfigPath()],
+})
